6a142a301029e56351bf9e0f4ec6d9728282cc59
Perry Jackson <76414839+indrixdev@users.noreply.github.com>
2022-06-23 13:33:50 +0100

    Create main.cpp

---
+++ Day 09/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "BankAccount.h"
+
+std::string input;
+
+int main()
+{
+
+    std::cout << "Would you like to open an account? ";
+    std::cin >> input;
+    if (input == "yes" || input == "Yes") {
+        BankAccount newAccount;
+        newAccount.makeNew();
+        std::cout << newAccount.accountNum << std::endl;
+        std::cout << newAccount.name << std::endl;
+        std::cout << newAccount.balance << std::endl;
+        std::cout << newAccount.status << std::endl;
+    }
+    else {
+        std::cout << "Incorrect input" << std::endl;
+    }
+    
+}
